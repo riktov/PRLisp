c27d55d5606b778c62a53cc80b06679a5cfa0efa
Paul N. Richter <nobuyuki.richter@gmail.com>
2016-02-09 22:27:22 +0900

    Added SystemProceduresTest

---
+++ tests/org/riktov/prlisp/SystemProceduresTest.java
@@ -0,0 +1,106 @@
+package org.riktov.prlisp;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SystemProceduresTest {
+	private Environment env;
+	@Before
+	public void setUp() {
+		env = new Environment();
+		env.initialize();
+	}
+
+	@Test
+	public void testLoad() {
+		//first write the temporary file
+		Writer writer = null ;
+		
+		String sourceFilePath = "testLoad.lisp" ;
+		
+		//create the test source file
+		try {
+			writer = new BufferedWriter(
+						new OutputStreamWriter(
+								new FileOutputStream(sourceFilePath), "utf-8")) ;
+			writer.write("(define foo 37)\n");
+			writer.write("(define bar 95)\n");
+		} catch (IOException except){
+			
+		} finally {
+			try {writer.close();} catch (Exception ex) {}
+		}
+		
+		//build a form
+		LispObject[] forms = new LispObject[] {
+				new SymbolAtom("load"),
+				new StringAtom(sourceFilePath)
+		} ;
+		
+		//convert to a list
+		ConsCell c = new ConsCell(forms) ;
+		
+		//evaluate it
+		System.out.println("testLoad() evaluating: " + c.toString()) ;
+		c.eval(env) ;
+	
+		//check that the environment contains the bindings as stated in the source file.
+		
+		System.out.println("testLoad(): got " + env.get("BAR")) ;
+
+		assertTrue(env.get("BAR").toString().equals("95"));
+	}
+
+	@Test
+	public void testLoadMultiline() {
+		//first write the temporary file
+		Writer writer = null ;
+		
+		String sourceFilePath = "testLoad2.lisp" ;
+		
+		//create the test source file
+		try {
+			writer = new BufferedWriter(
+						new OutputStreamWriter(
+								new FileOutputStream(sourceFilePath), "utf-8")) ;
+			writer.write("(define (add-thirteen x)\n");
+			writer.write("(+ x 13))\n");
+		} catch (IOException except){
+			
+		} finally {
+			try {writer.close();} catch (Exception ex) {}
+		}
+		
+		//build a form
+		LispObject[] forms = new LispObject[] {
+				new SymbolAtom("load"),
+				new StringAtom(sourceFilePath)
+		} ;
+		
+		//convert to a list
+		ConsCell c = new ConsCell(forms) ;
+		
+		//evaluate it
+		System.out.println("testLoad() evaluating: " + c.toString()) ;
+		c.eval(env) ;
+	
+		//check that the environment contains the bindings as stated in the source file.
+		
+		CompoundProcedure theProc = (CompoundProcedure) env.get("ADD-THIRTEEN") ;
+
+		forms = new LispObject[] { new ObjectAtom(4) } ;
+		c = new ConsCell(forms) ;
+		
+		LispObject result = theProc.apply(c) ;
+		
+		assertTrue(result.toString().equals("17"));
+	}
+}
